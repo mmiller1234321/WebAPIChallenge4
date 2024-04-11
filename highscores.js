f9437beb1b1339b071b2f979a7878c36658337ff
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-04-11 10:42:53 -0500

    revised js files

---
+++ highscores.js
@@ -0,0 +1,22 @@
+function printHighscores() {
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+  highscores.sort(function(a, b) {
+      return b.score - a.score;
+  });
+  highscores.forEach(function(score) {
+      var liTag = document.createElement("li");
+      liTag.textContent = score.name + " - " + score.score;
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+  });
+}
+
+function clearHighscores() {
+  window.localStorage.removeItem("highscores");
+  window.location.reload();
+}
+
+document.querySelector("#view-high-scores").onclick = printHighscores;
+document.getElementById("clear").onclick = clearHighscores;
+
+printHighscores();
